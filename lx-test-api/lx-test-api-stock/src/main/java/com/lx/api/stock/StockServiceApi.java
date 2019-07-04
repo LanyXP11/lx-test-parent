8579018aa0db7e4146306249c07447fd46b92748
chenjiang <chenjiang@hetrone.com>
2019-07-04 12:37:51 +0800

    no messages

--- lx-test-api/lx-test-api-stock/src/main/java/com/lx/api/stock/StockServiceApi.java
+++ lx-test-api/lx-test-api-stock/src/main/java/com/lx/api/stock/StockServiceApi.java
@@ -1,13 +1,15 @@
 package com.lx.api.stock;
 
 import com.lx.common.constant.LxResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * 库存服务对外api
- */
+@Api(tags = "库存服务")
 public interface StockServiceApi {
     /**
      * 减库存
@@ -15,7 +17,9 @@ public interface StockServiceApi {
      * @param commodityId
      * @return
      */
+    @ApiImplicitParams({@ApiImplicitParam(paramType = "query", required = true, name = "commodityId", dataType = "Long", value = "商品id")})
     @RequestMapping("/api/stock/inventoryReduction")
+    @ApiOperation("根据商品ID减库方法")
     public LxResponse inventoryReduction(@RequestParam("commodityId") Long commodityId);
 
 
